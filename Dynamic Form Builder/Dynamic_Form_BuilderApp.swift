e5289a877a97d8ac7c502a5e780eefe387a7ac41
Akash Sharma <sharmaakash8946@gmail.com>
2026-06-14 12:04:59 +0530

    Initial Commit

---
+++ Dynamic Form Builder/Dynamic_Form_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_Form_BuilderApp.swift
+//  Dynamic Form Builder
+//
+//  Created by Akash Sharma on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_Form_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
